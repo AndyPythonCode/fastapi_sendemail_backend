b1eda394dab78ff06e8f5c23c2583f4cc1a4d302
AndyPythonCode <andyarciniegas24@gmail.com>
2021-05-21 16:38:09 -0400

    database

--- database/db.py
+++ database/db.py
@@ -2,11 +2,12 @@ import sqlalchemy
 import databases
 from sqlalchemy.sql.schema import MetaData
 from databases.core import Database
+from decouple import config
 
 class Conexion():
     def __init__(self) -> None:
     
-        self.__DATABASE_URL = 'postgres://danzbsvvdhcxje:0257312b7f40189ee62b0b3541a480ef83b79b09bb01af5baa175c75a98ab9b9@ec2-184-73-198-174.compute-1.amazonaws.com:5432/dcbgjll9h0dq18'
+        self.__DATABASE_URL = config('DATABASE_URL')
         self.__database = databases.Database(self.__DATABASE_URL)
         self.__metadata = sqlalchemy.MetaData()
         self.__engine = sqlalchemy.create_engine(self.__DATABASE_URL)
